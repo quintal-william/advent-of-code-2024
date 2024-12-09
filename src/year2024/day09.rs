21daaee35c05fb27c91ae6a5b387ddefff2923f5
William <william@quintalwebsolutions.com>
2024-12-09 13:45:12 +0100

    Day 9

--- src/year2024/day09.rs
+++ src/year2024/day09.rs
@@ -2,8 +2,35 @@ use crate::solution::{Day, Solutions};
 
 pub struct Day09;
 
+type Id = usize;
+
+#[derive(Debug, Clone)]
+pub enum Block {
+    Empty(usize),
+    Used(Id, usize),
+}
+
+fn get_checksum(memory: Vec<Block>) -> u64 {
+    let mut score = 0u64;
+    let mut id = 0;
+    for block in memory {
+        match block {
+            Block::Used(value, size) => {
+                for _ in 0..size {
+                    score += (value * id) as u64;
+                    id += 1;
+                }
+            }
+            Block::Empty(size) => {
+                id += size;
+            }
+        }
+    }
+    return score;
+}
+
 impl Day for Day09 {
-    type Context = Vec<u8>;
+    type Context = Vec<Block>;
     type Part1 = u64;
     type Part2 = u64;
 
@@ -14,57 +41,99 @@ impl Day for Day09 {
     fn solutions() -> Solutions<Self::Part1, Self::Part2> {
         Solutions {
             part1_example: Some(1928),
-            part1: None,
-            part2_example: None,
-            part2: None,
+            part1: Some(6370402949053),
+            part2_example: Some(2858),
+            part2: Some(6398096697992),
         }
     }
 
     fn create_context(input: &str) -> Self::Context {
-        input
+        let parsed: Vec<usize> = input
             .chars()
-            .map(|c| c.to_digit(10).unwrap() as u8)
-            .collect()
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .collect();
+
+        let mut memory = vec![];
+        for (id, block) in parsed.iter().enumerate() {
+            let size = block.to_owned();
+            if size > 0 {
+                if id % 2 == 0 {
+                    memory.push(Block::Used(id / 2, size))
+                } else {
+                    memory.push(Block::Empty(size));
+                }
+            }
+        }
+        return memory;
     }
 
     fn solve_part1(context: &Self::Context) -> Self::Part1 {
-        let mut memory = vec![];
-        for (id, block) in context.iter().enumerate() {
-            for _ in 0..*block {
-                if id % 2 == 1 {
-                    memory.push(None);
-                } else {
-                    memory.push(Some(id / 2))
-                }
-            }
-        }
-
+        let mut memory = context.clone();
         let mut i = 0;
-        let mut j = memory.len();
-        while i != j && i < memory.len() {
-            if memory[i].is_none() {
-                while i != j {
-                    j -= 1;
-                    if let Some(Some(entry)) = memory.pop() {
-                        memory[i] = Some(entry);
-                        break;
+        let mut j = memory.len() - 1;
+        while i < j {
+            if let Block::Empty(empty_size) = memory[i] {
+                let mut size_left = empty_size;
+                let mut moved = vec![];
+                while i < j {
+                    if let Block::Used(value, size) = memory[j] {
+                        if size <= size_left {
+                            moved.push(Block::Used(value, size));
+                            memory[j] = Block::Empty(size);
+                            size_left -= size;
+                        } else {
+                            moved.push(Block::Used(value, size_left));
+                            memory[j] = Block::Used(value, size - size_left);
+                            break;
+                        }
                     }
+                    j -= 1;
                 }
+
+                let mut new_memory = memory[..i].to_vec();
+                new_memory.extend(moved.clone());
+                new_memory.extend_from_slice(&memory[i + 1..]);
+                memory = new_memory;
+
+                i += moved.len() - 1;
+                j += moved.len() - 1;
             }
             i += 1;
         }
-
-        let mut score = 0u64;
-        for (id, entry) in memory.iter().enumerate() {
-            if let Some(e) = *entry {
-                score += (id * e) as u64;
-            }
-        }
-
-        return score;
+        return get_checksum(memory);
     }
 
-    fn solve_part2(_context: &Self::Context) -> Self::Part2 {
-        0
+    fn solve_part2(context: &Self::Context) -> Self::Part2 {
+        let mut memory = context.clone();
+        let mut i = memory.len() - 1;
+        while i > 0 {
+            if let Block::Used(value, block_size) = memory[i] {
+                for j in 0..i {
+                    if let Block::Empty(empty_size) = memory[j] {
+                        if empty_size == block_size {
+                            memory[j] = Block::Used(value, block_size);
+                            memory[i] = Block::Empty(empty_size);
+                            break;
+                        } else if empty_size > block_size {
+                            memory[i] = Block::Empty(block_size);
+
+                            let mut new_memory = memory[..j].to_vec();
+                            new_memory.extend(vec![
+                                Block::Used(value, block_size),
+                                Block::Empty(empty_size - block_size),
+                            ]);
+                            new_memory.extend_from_slice(&memory[j + 1..]);
+                            memory = new_memory;
+
+                            i += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+            i -= 1;
+        }
+
+        return get_checksum(memory);
     }
 }
