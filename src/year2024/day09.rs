51044f13146a72c31770b2c30030e185a080b753
William <william@quintalwebsolutions.com>
2024-12-09 09:32:55 +0100

    Day 9 part 1

--- src/year2024/day09.rs
+++ src/year2024/day09.rs
@@ -3,29 +3,65 @@ use crate::solution::{Day, Solutions};
 pub struct Day09;
 
 impl Day for Day09 {
-    type Context = ();
-    type Part1 = i32;
-    type Part2 = i32;
+    type Context = Vec<u8>;
+    type Part1 = u64;
+    type Part2 = u64;
 
     fn title() -> String {
-        String::from("Title")
+        String::from("Disk Fragmenter")
     }
 
     fn solutions() -> Solutions<Self::Part1, Self::Part2> {
         Solutions {
-            part1_example: None,
+            part1_example: Some(1928),
             part1: None,
             part2_example: None,
             part2: None,
         }
     }
 
-    fn create_context(_input: &str) -> Self::Context {
-        ()
+    fn create_context(input: &str) -> Self::Context {
+        input
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect()
     }
 
-    fn solve_part1(_context: &Self::Context) -> Self::Part1 {
-        0
+    fn solve_part1(context: &Self::Context) -> Self::Part1 {
+        let mut memory = vec![];
+        for (id, block) in context.iter().enumerate() {
+            for _ in 0..*block {
+                if id % 2 == 1 {
+                    memory.push(None);
+                } else {
+                    memory.push(Some(id / 2))
+                }
+            }
+        }
+
+        let mut i = 0;
+        let mut j = memory.len();
+        while i != j && i < memory.len() {
+            if memory[i].is_none() {
+                while i != j {
+                    j -= 1;
+                    if let Some(Some(entry)) = memory.pop() {
+                        memory[i] = Some(entry);
+                        break;
+                    }
+                }
+            }
+            i += 1;
+        }
+
+        let mut score = 0u64;
+        for (id, entry) in memory.iter().enumerate() {
+            if let Some(e) = *entry {
+                score += (id * e) as u64;
+            }
+        }
+
+        return score;
     }
 
     fn solve_part2(_context: &Self::Context) -> Self::Part2 {
